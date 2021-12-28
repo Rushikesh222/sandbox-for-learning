44dee41e71a0849a2eca8c1535b4a8d3a05a4d4c
Rushikesh Shirsat <90494892+Rushikesh222@users.noreply.github.com>
2021-12-28 15:40:17 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite game</h1>
+    <ul>
+      <li>Cricket</li>
+      <li>FootBall</li>
+      <li>Volley ball</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
